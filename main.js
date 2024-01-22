a9d7fac2037535fc59b5750bb3fa3dc1de4b802a
Rodrigo <rvena@freecast.com>
2024-01-22 13:29:53 -0500

    testing

--- main.js
+++ main.js
@@ -7,13 +7,15 @@ const main = async () => {
   if (args.length < 2) {
     throw new Error('Invalid arguments. You must provide the media and output parameters.');
   }
-
+  var  ö___µ = "omg"
+  // console.log(results);
+  console.log(ö___µ);
+  //process.exit(1)
   const media = JSON.parse(args[0]).media;
   const outputLocation = args[2];
   const payload = args[1] || '';
   const results = await vudu_scraper.Init(media, payload, outputLocation);
-
-  console.log(results);
+  
 };
 
 main();
